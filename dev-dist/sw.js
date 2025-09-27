35d394461d3fb2a1cbe38ba8ed8261975aff0f09
IDGS-803-22001034 <80085@alumnos.utleon.edu.mx>
2025-09-26 19:46:38 -0600

    Correcciones

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qavrdsfgr4g"
+    "revision": "0.p6lo0rciue8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,18 @@ function App() {
     <>
       <div className="navbar shadow-sm" style={{ backgroundColor: "#000" }}>
         <div className="container-fluid">
-          <h1 className="text-center text-white w-100 my-2">Horóscopo</h1>
+          <h1 className="text-center text-white w-100 my-2 d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <img
+              src="https://img.freepik.com/vector-gratis/circulo-signos-zodiaco-astrologia_102902-2276.jpg?ga=GA1.1.1552816567.1751478748&semt=ais_hybrid&w=740&q=80"
+              alt="icono horóscopo"
+              style={{
+                width: "134px",
+                marginRight: "16px",
+                borderRadius: "12px",
+              }}
+            />
+            Horóscopo
+          </h1>
         </div>
       </div>
 
